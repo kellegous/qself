35de4a496be0e26c6695b67129f592d78b7ecba3
Kelly Norton <kellegous@gmail.com>
2015-01-13 20:27:59 -0500

    Renamed Rates to Stats

--- lib/src/heart/heart.go
+++ lib/src/heart/heart.go
@@ -1,11 +1,11 @@
 package heart
 
-type Rates struct {
+type Stats struct {
 	rrs []uint16
 	idx int
 }
 
-func (r *Rates) AddInterval(i uint16) {
+func (r *Stats) AddInterval(i uint16) {
 	n, c := len(r.rrs), cap(r.rrs)
 	if n < c {
 		r.rrs = append(r.rrs, i)
@@ -15,17 +15,17 @@ func (r *Rates) AddInterval(i uint16) {
 	}
 }
 
-func (r *Rates) Reset() {
+func (r *Stats) Reset() {
 	r.rrs = r.rrs[0:0]
 	r.idx = 0
 }
 
-func (r *Rates) Len() int {
+func (r *Stats) len() int {
 	return len(r.rrs)
 }
 
-func NewRates(n int) *Rates {
-	return &Rates{
+func NewStats(n int) *Stats {
+	return &Stats{
 		rrs: make([]uint16, 0, n),
 		idx: 0,
 	}
--- lib/src/heart/heart_test.go
+++ lib/src/heart/heart_test.go
@@ -5,17 +5,17 @@ import (
 )
 
 func TestCircularityOfRate(t *testing.T) {
-	r := NewRates(8)
+	r := NewStats(8)
 	for i := 0; i < 8; i++ {
-		if r.Len() != i {
-			t.Errorf("expected length of %d, got %d", i, r.Len())
+		if r.len() != i {
+			t.Errorf("expected length of %d, got %d", i, r.len())
 		}
 		r.AddInterval(uint16(i * 100))
 	}
 
 	for i := 0; i < 16; i++ {
-		if r.Len() != 8 {
-			t.Errorf("expected length of 8, got %d", r.Len())
+		if r.len() != 8 {
+			t.Errorf("expected length of 8, got %d", r.len())
 		}
 		r.AddInterval(uint16(i * 100))
 	}
